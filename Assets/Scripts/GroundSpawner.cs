2c2adbb1535b4d5fcb5b3d5af88d4543241e46db
Azin A <aalahkhani@gmail.com>
2023-12-20 18:30:55 +0000

    update

--- Assets/Scripts/GroundSpawner.cs
+++ Assets/Scripts/GroundSpawner.cs
@@ -5,12 +5,15 @@ public class GroundSpawner : MonoBehaviour
     public GameObject groundTile;
     Vector3 nextPoint;
 
+    public GameObject player;
+
     public void Spawn(){
         GameObject temp =  Instantiate(groundTile,nextPoint, Quaternion.identity);
         nextPoint = temp.transform.GetChild(1).transform.position;
     }
     void Start()
     {
+        player.SetActive(true);
         for (int i = 0; i < 5; i++)
         {
         Spawn();
--- Assets/Scripts/GroundTile.cs
+++ Assets/Scripts/GroundTile.cs
@@ -1,35 +1,47 @@
 
 using UnityEngine;
 
+
 public class GroundTile : MonoBehaviour
 {
     GroundSpawner groundSpawner;
-    
+
     void Start()
     {
         groundSpawner = GameObject.FindObjectOfType<GroundSpawner>();
         SpawnObstacle();
     }
-     private void OnTriggerExit(Collider other) {
+    private void OnTriggerExit(Collider other)
+    {
         groundSpawner.Spawn();
-         Destroy(gameObject,4);
+        Destroy(gameObject, 4);
     }
-  public GameObject obstaclePrefab;
-  public GameObject highObstaclePrefab;
+    public GameObject obstaclePrefab;
+    public GameObject highObstaclePrefab;
+    public GameObject FireObstacle;
 
-  void SpawnObstacle (){
-    
-    int obstaclePositionIndex = Random.Range(3,5);
-    int randomObstacle = Random.Range(0,2);
+    void SpawnObstacle()
+    {
 
-    Transform spawnPoint = transform.GetChild(obstaclePositionIndex).transform;
-    
-    if(randomObstacle==0){
-      Vector3 newSpawnPoints = new Vector3(0.14f, 1.41f , spawnPoint.position.z);
-    Instantiate(obstaclePrefab,newSpawnPoints,Quaternion.identity,transform);
-    } else {
-      Vector3 newSpawnPoints = new Vector3(0.14f, 6f , spawnPoint.position.z);
-      Instantiate(highObstaclePrefab,newSpawnPoints,Quaternion.identity,transform);
+        int obstaclePositionIndex = Random.Range(3, 5);
+        int randomObstacle = Random.Range(0, 3);
+
+        Transform spawnPoint = transform.GetChild(obstaclePositionIndex).transform;
+
+        if (randomObstacle == 0)
+        {
+            Vector3 newSpawnPoints = new Vector3(0.14f, 1.41f, spawnPoint.position.z);
+            Instantiate(obstaclePrefab, newSpawnPoints, Quaternion.identity, transform);
+        }
+        else if (randomObstacle == 1)
+        {
+            Vector3 newSpawnPoints = new Vector3(0.14f, 6f, spawnPoint.position.z);
+            Instantiate(highObstaclePrefab, newSpawnPoints, Quaternion.identity, transform);
+        }
+        else if (randomObstacle == 2)
+        {
+            Vector3 newSpawnPoints = new Vector3(0.14f, 1.41f, spawnPoint.position.z); 
+            Instantiate(FireObstacle, newSpawnPoints, Quaternion.identity, transform);
+        }
     }
-  }
 }
