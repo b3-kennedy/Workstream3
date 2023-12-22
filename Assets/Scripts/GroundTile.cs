a4ba51707a93e5e5a3dc56e95bcad23c798b76d5
Azin A <aalahkhani@gmail.com>
2023-12-22 19:55:56 +0000

    add new obstacle

--- Assets/Scripts/GroundTile.cs
+++ Assets/Scripts/GroundTile.cs
@@ -19,13 +19,14 @@ public class GroundTile : MonoBehaviour
     public GameObject obstaclePrefab;
     public GameObject highObstaclePrefab;
     public GameObject FireObstacle;
+    public GameObject secondHighObstaclePrefab;
 
-    
+
     public void SpawnObstacle()
     {
         
-            int obstaclePositionIndex = Random.Range(3, 5);
-            int randomObstacle = Random.Range(0, 3);
+            int obstaclePositionIndex = Random.Range(3, 7);
+            int randomObstacle = Random.Range(0, 4);
         Debug.Log(randomObstacle);
 
             Transform spawnPoint = transform.GetChild(obstaclePositionIndex).transform;
@@ -45,6 +46,13 @@ public class GroundTile : MonoBehaviour
                 Vector3 newSpawnPoints = new Vector3(0.14f, 2.14f, spawnPoint.position.z);
                 Instantiate(FireObstacle, newSpawnPoints, Quaternion.identity, transform);
             }
+            else if(randomObstacle == 3)
+        {
+
+            Vector3 newSpawnPoints = new Vector3(0.14f, 6.9f, spawnPoint.position.z);
+            Instantiate(secondHighObstaclePrefab, newSpawnPoints, Quaternion.identity, transform);
+            
+        }
            
 
        
